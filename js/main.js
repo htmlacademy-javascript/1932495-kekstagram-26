ea63cd60625f68fc20bfb5c3f6015aaa166a5ce5
prokhanova-k <kristina_kretova@list.ru>
2022-06-09 11:50:28 +0300

    Добавление вспомогательных функций №2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 function getRandomNumber (min,max) {
-  if ((min || max < 0) || (max < min)) {
-      alert('Ошибка!')
-      return;
+  if (min < 0 || max < 0)  {
+    throw new Error ('Введите число больше нуля!');
   }
-  else {
+  if (max <= min) {
+    throw new Error ('Звчение "до" должно быть больше значения "от"!');
+  }
   return Math.floor(Math.random()*(max-min+1))+min;
-}}
+}
+
+getRandomNumber(5,20);
 
 function getLengthString (checkString, maxLength) {
   if (checkString.length < maxLength) {
-     return true;
+    return true;
   }
   return false;
 }
+
+getLengthString('7',89);
