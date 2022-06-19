50ee19718f50c3248b23a9c45938ce8cf3586829
prokhanova-k <kristina_kretova@list.ru>
2022-06-19 15:23:26 +0300

    Больше деталей.Доработка

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable indent */
-// const { name } = require("browser-sync");
+// const { name } = require("browser-sync"); //я все таки не понимаю, что это такое, как оно появилось в коде?
 
-function getRandomInteger (min,max) {
+const getRandomInteger = (min,max) => {
   if (min < 0 || max < 0)  {
     throw new Error ('Введите число больше нуля!');
   }
@@ -9,7 +9,7 @@ function getRandomInteger (min,max) {
     throw new Error ('Значение "до" должно быть больше значения "от"!');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRandomInteger(5,20);
 
@@ -17,7 +17,7 @@ const checkTextLength = (text, maxLength) => text.length <= maxLength;
 
 checkTextLength('7',89);
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'В лесу',
   'В деревне',
   'На печке',
@@ -39,7 +39,7 @@ const NAMES = [
   'Василиса',
 ];
 
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -49,29 +49,29 @@ const MESSAGE = [
 ];
 
 
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+const getRandomPositiveInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+const getRandomArrayElement = (items) => items[getRandomPositiveInteger(0, items.length - 1)];
 
 let commentIdCounter = 1;
 
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
+const shuffleArray = (items) => {
+  for (let i = items.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [items[i], items[j]] = [items[j], items[i]];
   }
-  return array;
+  return items;
 };
 
-const createPhotoComments = () => ({
+const createPhotoComment = () => ({
       id: commentIdCounter++,
       avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
-      message: shuffleArray(MESSAGE).slice(0, getRandomPositiveInteger(1, 2)),
+      message: shuffleArray(MESSAGES).slice(0, getRandomPositiveInteger(1, 2)).join(' '),
       name: getRandomArrayElement(NAMES),
     });
 
@@ -82,12 +82,13 @@ const createPhotos = () => {
     photos.push({
       id: i,
       url: `photos/${i}.jpg`,
-      description: getRandomArrayElement(DESCRIPTION),
+      description: getRandomArrayElement(DESCRIPTIONS),
       likes: getRandomPositiveInteger(15, 200),
-      comments: Array.from({length: 4}, createPhotoComments)
+      comments: Array.from({length: 4}, createPhotoComment)
     });
   }
   return photos;
 };
 
-createPhotos();
+const somePhotos = createPhotos(25);
+somePhotos();
